39b3102c0d1e7d2d6a944230813e031c0782efd6
reivenw <waltercorrea64@gmail.com>
2022-03-11 19:15:40 -0400

    1

--- assets/global.js
+++ assets/global.js
@@ -1,3 +1,9 @@
+$('#slider1').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
+
 function getFocusableElements(container) {
   return Array.from(
     container.querySelectorAll(
